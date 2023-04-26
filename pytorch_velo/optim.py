cc645bd9b7465fc56799fc9dc5b1efcea70978d5
lxy <1304043543@qq.com>
2023-04-26 22:31:15 +0800

    change device？

--- pytorch_velo/optim.py
+++ pytorch_velo/optim.py
@@ -185,6 +185,7 @@ class VeLO(th.optim.Optimizer):
             loss = _th_to_jax(loss)
         else:
             loss = jnp.array(0.0)
+            jax.device_put(loss)
 
         self.state['rng_key'], opt_key = jax.random.split(
             self.state['rng_key'])
