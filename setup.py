b8e8b37ebd1dfcef7f52480141fc9db06526e752
lxy <1304043543@qq.com>
2023-04-26 19:03:55 +0800

    暂时注销依赖

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     python_requires='>=3.8',
     version='0.0.1',
     packages=['pytorch_velo'],
-    install_requires=[
-        (
-            'learned_optimization '
-            '@ git+https://github.com/google/learned_optimization.git'
-        ),
-        'optax>=0.1',
-        'torch',
-    ],
+    # install_requires=[
+    #     (
+    #         'learned_optimization '
+    #         '@ git+https://github.com/google/learned_optimization.git'
+    #     ),
+    #     'optax>=0.1',
+    #     'torch',
+    # ],
     author='Jan Ebert',
     author_email='janpublicebert@posteo.de',
 )
